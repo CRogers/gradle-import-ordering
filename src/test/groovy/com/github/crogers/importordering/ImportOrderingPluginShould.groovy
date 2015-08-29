503a96e91fbf6aca507bd19ec59666ab382cfa4a
Callum Rogers <callumrogers@hotmail.co.uk>
2015-08-29 16:36:06 +0100

    Create first failing test

---
+++ src/test/groovy/com/github/crogers/importordering/ImportOrderingPluginShould.groovy
@@ -0,0 +1,48 @@
+package com.github.crogers.importordering
+
+import groovy.transform.CompileStatic
+import org.gradle.testkit.runner.GradleRunner
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TemporaryFolder
+import org.w3c.dom.Document
+
+import javax.xml.parsers.DocumentBuilder
+import javax.xml.parsers.DocumentBuilderFactory
+
+import static org.hamcrest.MatcherAssert.assertThat
+import static org.hamcrest.xml.HasXPath.hasXPath
+
+@CompileStatic
+public class ImportOrderingPluginShould {
+    @Rule public final TemporaryFolder projectDir = new TemporaryFolder();
+    private File buildFile;
+
+    @Before
+    public void createBuildFile() throws IOException {
+        buildFile = projectDir.newFile("build.gradle");
+    }
+
+    @Test
+    public void produce_a_single_entry_in_the_ipr_xml_with_a_single_entry() {
+        buildFile << """
+            apply plugin: 'idea'
+            apply plugin: 'import-ordering'
+
+            importOrdering {
+                importLine "foo.bar.*"
+            }
+        """.stripIndent()
+
+        GradleRunner.create()
+            .withProjectDir(projectDir.getRoot())
+            .withArguments("idea")
+            .build()
+
+        DocumentBuilder documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder()
+        Document doc = documentBuilder.parse(new File(projectDir.getRoot(), "foo.ipr"))
+
+        assertThat(doc, hasXPath("/some/crazy/xpath"))
+    }
+}
